77ffccf6550453a5eb42e3e2d347b8f8444cf87e
Francisco Oteo <panchyn.oteo@gmail.com>
2022-06-29 20:04:21 -0300

    segundo desafio coder

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,20 @@
-function ingresoPersona(nombre, apellido){
-    let saludo = `Bienvenida a ShowroomFreyja ${nombre} ${apellido}. Aqui podras encontrar el oufit que siempre buscaste`;
-    alert(saludo);
-}
+function tarjetas(){
+    let nombreTarjetaCredito = prompt("Ingrese su tarjeta de credito:");
 
-let nombrePer = prompt("ingresa tu nombre");
-let apellidoPer = prompt("Ingrese su apellido");
-ingresoPersona(nombrePer, apellidoPer);
+    if (nombreTarjetaCredito == "visa"){
+        alert("Podes abonar en 1, 3, 6 o 12 cuotas sin interes.");
+    } else if (nombreTarjetaCredito == "mastercard"){
+        alert("Podes abonar en 1, 3, 6 o 12 cuotas sin interes.");
+    } else if (nombreTarjetaCredito == "naranja"){
+        alert("Podes abonar en 3 y 6 cuotas sin interes o 12 cuotas fijas.");
+    } else if (nombreTarjetaCredito == "mercadopago"){
+        alert("Si abonas por mercardo pago, tenes un 10% de descuento en la compra de cualquier producto.");
+    } 
+    else {
+        alert("Otras tarjetas, en 3, 6 y 12 cuotas fijas.");
+    }
+}
+tarjetas()
 
 class Oufit{
     constructor (nombre, precio, img, envioGratis){
